210401365bd3047a9453e19c7db086c13b8f1986
Uli Roth <uli@shad0wfax.de>
2023-02-01 08:14:04 +0100

    added RHO to HDF output

--- include/output_single.h
+++ include/output_single.h
@@ -9,7 +9,9 @@
      &            S_STEP(NMAX), S_STEPR(NMAX), S_T0(NMAX), S_T0R(NMAX),
      &                  S_ASPN(NMAX), S_RS(NMAX), S_L(NMAX), S_TE(NMAX),
      &                  S_RC(NMAX), S_MC(NMAX), S_POT(NMAX),
-     &                  NS_KW(NMAX),NS_NAM(NMAX),NS_LAB(NMAX)
+     &                  NS_KW(NMAX),NS_NAM(NMAX),NS_LAB(NMAX),
+     &                  S_RHO(NMAX)
+
       REAL*4 S_AS, S_M, S_RSN, S_X1, S_X2, S_X3, S_V1, S_V2, S_V3
       REAL*4 S_F1, S_F2, S_F3
       REAL*4 S_FD1, S_FD2, S_FD3
@@ -17,5 +19,5 @@
       REAL*4 S_D31, S_D32, S_D33
       REAL*4 S_STEP, S_STEPR, S_T0, S_T0R, S_ASPN
       REAL*4 S_RS, S_L, S_TE, S_RC, S_MC
-      REAL*4 S_POT
+      REAL*4 S_POT, S_RHO
       INTEGER NS_KW,NS_NAM,NS_LAB
